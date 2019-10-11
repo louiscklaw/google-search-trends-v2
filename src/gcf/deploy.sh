94fe92654efb44e3492c422b6aff30301337230c
louiscklaw <louiscklaw@gmail.com>
2019-10-11 16:03:45 +0800

    working update,

--- src/gcf/deploy.sh
+++ src/gcf/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 gcloud functions deploy google-trends-crawler --entry-point main_routes --runtime nodejs8 --trigger-http --project louislabs-com
+curl -vLk https://us-central1-louislabs-com.cloudfunctions.net/google-trends-crawler
